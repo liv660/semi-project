f73c0784408d0f28ac028bac6e7c62a2959ac274
zimmyrabbit <zimmyrabbit@naver.com>
2021-04-22 12:30:14 +0900

    NoticeComment DTO

--- SemiProject/src/dto/NoticeComment.java
+++ SemiProject/src/dto/NoticeComment.java
@@ -10,47 +10,84 @@ public class NoticeComment {
 	private String nick;
 	private String commentText;
 	private Date commentDate;
+	
+	//userimg JOIN
+	private String storedName;
+	
+	//total comment count
+	private int commentCnt;
+
 	@Override
 	public String toString() {
 		return "NoticeComment [commentNo=" + commentNo + ", noticeNo=" + noticeNo + ", userNo=" + userNo + ", nick="
-				+ nick + ", commentText=" + commentText + ", commentDate=" + commentDate + "]";
+				+ nick + ", commentText=" + commentText + ", commentDate=" + commentDate + ", storedName=" + storedName
+				+ ", commentCnt=" + commentCnt + "]";
 	}
+
 	public int getCommentNo() {
 		return commentNo;
 	}
+
 	public void setCommentNo(int commentNo) {
 		this.commentNo = commentNo;
 	}
+
 	public int getNoticeNo() {
 		return noticeNo;
 	}
+
 	public void setNoticeNo(int noticeNo) {
 		this.noticeNo = noticeNo;
 	}
+
 	public int getUserNo() {
 		return userNo;
 	}
+
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
+
 	public String getNick() {
 		return nick;
 	}
+
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
+
 	public String getCommentText() {
 		return commentText;
 	}
+
 	public void setCommentText(String commentText) {
 		this.commentText = commentText;
 	}
+
 	public Date getCommentDate() {
 		return commentDate;
 	}
+
 	public void setCommentDate(Date commentDate) {
 		this.commentDate = commentDate;
 	}
+
+	public String getStoredName() {
+		return storedName;
+	}
+
+	public void setStoredName(String storedName) {
+		this.storedName = storedName;
+	}
+
+	public int getCommentCnt() {
+		return commentCnt;
+	}
+
+	public void setCommentCnt(int commentCnt) {
+		this.commentCnt = commentCnt;
+	}
 	
+
 	
 }
